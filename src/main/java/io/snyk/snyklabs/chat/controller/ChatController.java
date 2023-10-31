7c2b0e3b1ac820309edca067ce42a375497c7412
JieShi-HS <jieshi.tan@handshakes.com.my>
2023-10-31 15:17:30 +0800

    fix bugs

--- src/main/java/io/snyk/snyklabs/chat/controller/ChatController.java
+++ src/main/java/io/snyk/snyklabs/chat/controller/ChatController.java
@@ -100,7 +100,8 @@ public class ChatController {
     @GetMapping("/hello")
     public void hello(@RequestParam String user, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        response.getWriter().write("<h1>Hello: " + user + "</h1>");
+        String safeUser = HtmlUtils.htmlEscape(user);
+        response.getWriter().write("<h1>Hello: " + safeUser + "</h1>");
         response.getWriter().flush();
     }
 
